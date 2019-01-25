f42a67303a3e53dae61a26738234d094392b0384
imanuelvicky <inv484@gmail.com>
2019-01-25 13:12:02 +0700

    done sisa deploy

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,24 +2,27 @@
     <div class="container">
         <b-row>
             <b-col>
-                <h1>Player1</h1>
-                <h3>score: {{room.player1score}}</h3>
+                <h1>{{room.player1}}</h1>
+                <h3>score: {{room.score1}}</h3>
                 <a @click="play1"><img width="70%" src="https://cdn0.iconfinder.com/data/icons/summer-2/512/Summer_512px-14.png"/></a>
             </b-col>
             <img src="https://png.pngtree.com/svg/20170314/the_vertical_line_270296.png"/>
             <b-col>
-                <h1>Player2 </h1>
-                <h3>score: {{room.player2score}}</h3>
+                <h1>{{room.player2}} </h1>
+                <h3>score: {{room.score2}}</h3>
                 <a @click="play2"><img width="70%" src="https://image.flaticon.com/icons/png/512/194/194630.png"/></a>
             </b-col>
         </b-row>
     </div>
 </template>
 <script>
+
 export default {
   data () {
     return {
-        room: Object
+        room: Object,
+        score1: 0,
+        score2: 0
     }
   },
   methods: {
@@ -28,12 +31,12 @@ export default {
             var audio = new Audio("http://s1download-universal-soundbank.com/mp3/sounds/147.mp3");
             audio.play()
             .then((data) => {
-                console.log(this.player1score)
                 if (this.room.score1 < 20) {
-                    if (this.room.score2 === 20) {
+                    if (this.room.score2 === 21) {
                         this.lose()
                     } else {
-                        this.$store.dispatch('updateScore', { id:this.$store.params.id, key: "score1"} )
+                        this.score1++
+                        this.$store.dispatch('updateScore', { id:this.room.id, score: this.score1, type: 'score1'} )
                     }
                 }else {
                    this.win()
@@ -47,18 +50,20 @@ export default {
         }
     },
     play2 () {
-        if (localStorage.getItem('myname') === this.room.player2) {
+        if (localStorage.getItem('name') === this.room.player2) {
             var audio = new Audio("http://www.orangefreesounds.com/wp-content/uploads/2018/04/Dog-woof-single-sound.mp3?_=1");
             audio.play()
             .then((data) => {
                 if (this.room.score2 < 20) {
-                    if (this.room.score2 === 20) {
+                    if (this.room.score1 === 21) {
                         this.lose()
                     } else {
-                        this.$store.dispatch('updateScore', { id:this.$store.params.id, name: "player2"} )
+                        this.score2++
+                        this.$store.dispatch('updateScore', { id: this.room.id, score: this.score2, type: 'score2'} )
                     }
                 } else {
                     this.win()
+
                 }
             })
             .catch((err) => {
@@ -72,25 +77,70 @@ export default {
       swal({
         icon: "success",
         text: "YOU WIN !!!!"
-      });
+      })
+    //   .then(() => {
+    //         router.push('/')
+    //   })
+    
+    this.$store.dispatch('exitMe')
+    //   this.lose();
+    //   this.$store.dispatch('reset', this.room.id);
     },
     lose () {
       swal ({
         icon:"error",
         text: "BIG LOSER !!!!!"
       })
+      this.$store.dispatch('reset', this.room.id);
     }
   },
   mounted() {
-    if(!localStorage.getItem("myname")) {
+    if(!localStorage.getItem("name")) {
       this.$router.push('/')
     } else {
-      let id = this.$store.params.id
+      let id =  this.$route.params.id 
       let myroom = this.$store.state.all_room
-      myroom.filter((r) => {
-          return r.id === id
-      })
-      this.room = myroom
+    //   myroom.filter((el) => {
+    //       if (el.id === id) {
+    //           return el
+    //       }
+    //   }
+    let newRoom = myroom.filter(function(e) {
+      if (e.id == id) {
+          return e
+      }
+    })
+      this.room = newRoom[0]
+    }
+  },
+  watch: {
+    score2: function (val) {
+        if(!localStorage.getItem("name")) {
+           this.$router.push('/')
+        } else {
+            let id =  this.$route.params.id 
+            let myroom = this.$store.state.all_room
+            let newRoom = myroom.filter(function(e) {
+                if (e.id == id) {
+                return e
+                }
+            })
+            this.room = newRoom[0]
+        }
+    },
+    score1: function (val) {
+        if(!localStorage.getItem("name")) {
+           this.$router.push('/')
+        } else {
+            let id =  this.$route.params.id 
+            let myroom = this.$store.state.all_room
+            let newRoom = myroom.filter(function(e) {
+                if (e.id == id) {
+                return e
+                }
+            })
+            this.room = newRoom[0]
+        }
     }
   }
 }
